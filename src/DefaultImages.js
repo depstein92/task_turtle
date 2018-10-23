3264f69c13d391cdada42af255844371d0a62d94
Daniel Epstein <d.epstein92@gmail.com>
2018-10-23 09:14:47 -0400

    added file-loader to webpack

---
+++ src/DefaultImages.js
@@ -0,0 +1,36 @@
+const defaultImages = [
+  {
+    title: 'Distracted Boyfriend',
+    image_url: './style/images/Distracted-Boyfriend.jpg'
+  },
+  {
+    title: 'Exapanding-Brain.jpg',
+    image_url: './style/images/Expanding-Brain.jpg'
+  },
+  {
+    title: 'Batman Slapping Robin',
+    image_url: './style/images/batman_robin.jpg'
+  },
+  {
+    title: 'Is-This-A-Pigeon',
+    image_url: 'Is-This-A-Pigeon.jpg'
+  },
+  {
+    title: 'Left Exit Quickly',
+    image_url: 'Left-Exit-12-Off-Ramp.jpg'
+  },
+  {
+    title: 'Mocking Spongebob',
+    image_url: 'Mocking-Spongebob'
+  },
+  {
+    title: 'Roll Safe and Think About it',
+    image_url: 'Roll-Safe-and-Think-About-it.jpg'
+  },
+  {
+    title: 'Waiting Skeleton',
+    image_url: 'Waiting-Skeleton'
+  },
+]
+
+export default defaultImages;
--- src/containers/Preview.js
+++ src/containers/Preview.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import defaultImages from '../DefaultImages';
 
 class Preview extends React.Component{
   render(){
     return(
       <div className="preview-container">
-
+        
       </div>
     )
   }
