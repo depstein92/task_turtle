fc6e1a57b193b10ac0eb79decd36193faa780081
Daniel Epstein <d.epstein92@gmail.com>
2018-11-03 16:07:10 -0400

    made basic header in Heading Component

---
+++ src/ReusableStyles.js
@@ -0,0 +1,16 @@
+
+
+
+
+const primaryFont = {
+  fontFamily: "Spicy Rice",
+  fontStyle: "normal",
+  fontWeight: "normal",
+  src: "https://fonts.googleapis.com/css?family=Spicy+Rice"
+};
+
+
+
+
+
+export default { primaryFont: primaryFont };
--- src/containers/Heading.js
+++ src/containers/Heading.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { StyleSheet, css } from "aphrodite";
+import globalStyles from "../ReusableStyles";
 
 class Heading extends React.Component{
   render(){
     return(
-       <div className="heading">
-      I am heading
+      <div className={css(styles.heading_container)}>
+       <div className={css(styles.link)}>
+         <Link to="/">Create Meme</Link>
        </div>
+       <div className={css(styles.link)}>
+         <Link to="/">Blank Link</Link>
+       </div>
+       <div className={css(styles.link)}>
+         <Link to="/">Blank Link</Link>
+       </div>
+       <div className={css(styles.link)}>
+         <Link to="/">Blank Link</Link>
+       </div>
+      </div>
     )
   }
 }
+
+const styles = StyleSheet.create({
+   heading_container: {
+    width: '100%',
+    backgroundColor: 'blue',
+    height: '15vh',
+    borderBottom:'5px solid',
+    marginBottom: '30px',
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "flex-end"
+   },
+   link: {
+     fontFamily: globalStyles.primaryFont.fontFamily,
+     color: "inherit",
+     cursor: "pointer",
+     fontSize: "3vh"
+   }
+});
 
 export default Heading;
