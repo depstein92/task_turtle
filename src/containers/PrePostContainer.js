f1fe9144b771ad2f0b3b794ac8345f926ad4d7b3
Daniel Epstein <d.epstein92@gmail.com>
2018-11-21 12:12:08 -0500

    added image from Editor Container to PrePost Container

--- src/containers/PrePostContainer.js
+++ src/containers/PrePostContainer.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { connect } from 'react-redux';
+import { Image } from'semantic-ui-react';
 
 
 class PrePostContainer extends React.Component{
   constructor(props){
     super(props);
-    this.state={ editorInfo: [{ values: 'editor info not avalible' }] }
+    this.state = {
+      editorInfo: [{ values: 'editor info not avalible' }]
+    }
     this.renderEditorInfo = this.renderEditorInfo.bind(this);
   }
 
@@ -24,6 +27,12 @@ class PrePostContainer extends React.Component{
 
   renderEditorInfo(){
     let { editorInfo } = this.state,
+        { addImageToEditor: data } = this.props,
+
+        image = Object.keys(data).length === 0 ?
+                <div></div> :
+                <Image src={data.data.image} size="large" centered />,
+
         narrative = editorInfo[1] === undefined ?
                     <div>{ editorInfo[0].values }</div> :
                     Object.values(editorInfo[1].values)
@@ -32,12 +41,14 @@ class PrePostContainer extends React.Component{
                           });
     return(
       <div>
+        { image }
         { narrative }
       </div>
     )
   }
 
   render(){
+    console.log(this.props.addImageToEditor)
     return(
       <div className={css(styles.prepost_container)}>
        {this.renderEditorInfo() }
@@ -56,7 +67,8 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = (state) => {
   return {
-    formData: state.form
+    formData: state.form,
+    addImageToEditor: state.addImageReducer,
   }
 }
 
